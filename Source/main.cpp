b99715d61549262934cb140f709a1301e1d20047
MovementGH <48330137+MovementGH@users.noreply.github.com>
2020-01-17 05:33:59 -0500

    Add Demo Option

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,7 +4,7 @@
 #include "../SelbaWard/SelbaWard.hpp"
 
 int main() {
-    sf::Vector2u Size(1280,1024);
+    sf::Vector2u Size(1920,1080);
 
     sf::RenderWindow Window;
     sf::ContextSettings Settings;
@@ -12,6 +12,7 @@ int main() {
     Window.create(sf::VideoMode(Size.x,Size.y),"Test",sf::Style::Default,Settings);
     Window.setVerticalSyncEnabled(true);
 
+    #ifdef Demo3D
     LiveVisualizer Visualizer;
     Visualizer.setRenderSize(Size/(unsigned)2);
     Visualizer.setOutputSize({Size.y/2,Size.x/2},90,1);
@@ -24,25 +25,24 @@ int main() {
     Visualizer.addPlugin(new Plugin::Amplitude(4,1280,1,{128,128,128},{128,128,128},0,0.05,1),{0,1},{{{0,.75*Size.y/2,Size.x/2,Size.y/8},0},{{0,0,Size.x/2,Size.y/8},180}});
     Visualizer.addPlugin(new Plugin::Hanning(512,sf::Color::White),{0,1},{{{0,Size.y/8,Size.x/2,Size.y/4},0}});
     Visualizer.addPlugin(new Plugin::Pitch(2048,sf::Color::White,sf::Color::White,true,3),{0,1},{{{0,.75*Size.y/2,Size.x/2,Size.y/8},0},{{0,0,Size.x/2,Size.y/8},180}});
-    Visualizer.Play();
-    
     sw::WarpField Warp;
     Warp.setSize({Size.x,Size.y});
     Warp.setStarCount(20000);
     Warp.setColor(sf::Color::White);
-
     sw::Hexahedron Hexahedron({Size.y/2.f,Size.x/2.f,Size.x/2.f},2);
     Hexahedron.rotate({90,0,90});
     for(int i=0;i<4;i++)
         Hexahedron.setTexture(sw::Hexahedron::Face(i),sw::Hexahedron::Side::Outside,Visualizer.getTexture());
     Hexahedron.setPosition({Size.x/2,Size.y/2});
-    
+    sf::Clock Clock;
+    Visualizer.Play();
     while(Window.isOpen()) {
+        std::cout<<(int)(1.f/Clock.restart().asSeconds())<<std::endl;
         sf::Event Event;
         while(Window.pollEvent(Event)) {
             if(Event.type==sf::Event::Closed) Window.close();
         }
-        Hexahedron.rotate({0.5,.1,0});
+        Hexahedron.rotate({0.5,0,0});
         Warp.move(3);
         Window.clear();
         Window.draw(Warp);
@@ -50,4 +50,31 @@ int main() {
         Hexahedron.draw(&Window);
         Window.display();
     }
+    #else
+    LiveVisualizer Visualizer;
+    Visualizer.setRenderSize(Size);
+    Visualizer.setOutputSize(Size,0,1);
+    Visualizer.addPlugin(new Plugin::ConfettiCircles({1,10},{15,85},{.1,1},{sf::Color::White,{0,0,0,0}},{{255,255,255},{0,0,0,0}},{0,0},{99,0},{1,4},1.2,10),{0,1},{{{0,0,Size.x,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::Lasers(1.2,{1,0},{1,0},{2,0},{2,0},{sf::Color::White,{0,0,0,0}},10,"Product/Grad.png"),{0,2},{{{0,0,Size.x,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::VolumeCircle(4,sf::Color::Black,{255,255,255,255},2),{0,1},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::VolumeCircle(2,sf::Color::Black,{255,255,255,255},2),{0,.5},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::VolumeCircle(1,sf::Color::Black,{255,255,255,255},2),{0,.25},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::VolumeCircle(.5,sf::Color::Black,{255,255,255,255},2),{0,.125},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
+    Visualizer.addPlugin(new Plugin::Amplitude(4,1280,1,{128,128,128},{128,128,128},0,0.05,1),{0,1},{{{0,.75*Size.y,Size.x,Size.y/4},0},{{0,0,Size.x,Size.y/4},180}});
+    Visualizer.addPlugin(new Plugin::Hanning(512,sf::Color::White),{0,1},{{{0,Size.y/4,Size.x,Size.y/2},0}});
+    Visualizer.addPlugin(new Plugin::Pitch(2048,sf::Color::White,sf::Color::White,true,3),{0,1},{{{0,.75*Size.y,Size.x,Size.y/4},0},{{0,0,Size.x,Size.y/4},180}});
+    sf::Sprite Sprite;
+    Sprite.setTexture(Visualizer.getTexture());
+    Visualizer.Play();
+    while(Window.isOpen()) {
+        sf::Event Event;
+        while(Window.pollEvent(Event)) {
+            if(Event.type==sf::Event::Closed) Window.close();
+        }
+        Window.clear();
+        Visualizer.render();
+        Window.draw(Sprite);
+        Window.display();
+    }
+    #endif
 }
--- build.sh
+++ build.sh
@@ -1,7 +1,1 @@
-mkdir Build
-cd Build
-g++ -c -g ../Source/*.cpp -w
-g++ -c -g ../SelbaWard/Sprite3d.cpp -w
-g++ *.o -g -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system -o ../Product/Visualizer
-cd ..
-rm Build -r
+g++ -Ofast -g -w Source/*.cpp SelbaWard/Sprite3d.cpp -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system -o Product/Visualizer
