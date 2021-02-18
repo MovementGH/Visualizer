a146440644bdc505a979d9436e6308c000b0fc21
MovementGH <48330137+MovementGH@users.noreply.github.com>
2021-02-18 12:01:34 -0500

    Update Demo

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
-#include "Platform.hpp"
 #include "Inputs.hpp"
 #include "../SelbaWard/SelbaWard.hpp"
 
-int main() {
+int main(int argc,char** argv) {
     sf::Vector2u Size(1920,1080);
+    int Input=0;
+    if(argc>1)
+        Input=std::atoi(argv[1]);
 
     sf::RenderWindow Window;
     sf::ContextSettings Settings;
@@ -12,8 +14,10 @@ int main() {
     Window.create(sf::VideoMode(Size.x,Size.y),"Test",sf::Style::Default,Settings);
     Window.setVerticalSyncEnabled(true);
 
-    #ifdef Demo3D
     LiveVisualizer Visualizer;
+    Visualizer.setDevice(Visualizer.getAvailableDevices()[Input]);
+    std::cout<<"Recording from "<<Visualizer.getAvailableDevices()[Input]<<std::endl;
+    Visualizer.setAmplification(argc>2?std::atoi(argv[2]):1.f);
     Visualizer.setRenderSize(Size/(unsigned)2);
     Visualizer.setOutputSize({Size.y/2,Size.x/2},90,1);
     Visualizer.addPlugin(new Plugin::ConfettiCircles({1,10},{15,85},{.1,1},{sf::Color::White,{0,0,0,0}},{{255,255,255},{0,0,0,0}},{0,0},{99,0},{1,4},1.2,10),{0,1},{{{0,0,Size.x/2,Size.y/2},0}});
@@ -22,9 +26,9 @@ int main() {
     Visualizer.addPlugin(new Plugin::VolumeCircle(2,sf::Color::Black,{255,255,255,255},2),{0,.5},{{{-(int)Size.y/4,0,Size.y/2,Size.y/2},0},{{Size.x/2-Size.y/4,0,Size.y/2,Size.y/2},0}});
     Visualizer.addPlugin(new Plugin::VolumeCircle(1,sf::Color::Black,{255,255,255,255},2),{0,.25},{{{-(int)Size.y/4,0,Size.y/2,Size.y/2},0},{{Size.x/2-Size.y/4,0,Size.y/2,Size.y/2},0}});
     Visualizer.addPlugin(new Plugin::VolumeCircle(.5,sf::Color::Black,{255,255,255,255},2),{0,.125},{{{-(int)Size.y/4,0,Size.y/2,Size.y/2},0},{{Size.x/2-Size.y/4,0,Size.y/2,Size.y/2},0}});
-    Visualizer.addPlugin(new Plugin::Amplitude(4,1280,1,{128,128,128},{128,128,128},0,0.05,1),{0,1},{{{0,.75*Size.y/2,Size.x/2,Size.y/8},0},{{0,0,Size.x/2,Size.y/8},180}});
+    Visualizer.addPlugin(new Plugin::Amplitude(4,1280,1,{128,128,128},{128,128,128},.025*Size.y/2,0.05,1),{0,1},{{{0,.75*Size.y/2,Size.x/2,Size.y/8},0},{{0,0,Size.x/2,Size.y/8},180}});
     Visualizer.addPlugin(new Plugin::Hanning(512,sf::Color::White),{0,1},{{{0,Size.y/8,Size.x/2,Size.y/4},0}});
-    Visualizer.addPlugin(new Plugin::Pitch(2048,sf::Color::White,sf::Color::White,true,3),{0,1},{{{0,.75*Size.y/2,Size.x/2,Size.y/8},0},{{0,0,Size.x/2,Size.y/8},180}});
+    Visualizer.addPlugin(new Plugin::Pitch(4096,sf::Color::White,sf::Color::Red,true,3),{0,1},{{{0,Size.y/4,Size.x/2,Size.y/4},0},{{0,0,Size.x/2,Size.y/4},180}});
     sw::WarpField Warp;
     Warp.setSize({Size.x,Size.y});
     Warp.setStarCount(20000);
@@ -37,12 +41,12 @@ int main() {
     sf::Clock Clock;
     Visualizer.Play();
     while(Window.isOpen()) {
-        std::cout<<(int)(1.f/Clock.restart().asSeconds())<<std::endl;
+	float Speed=Clock.restart().asSeconds()/0.016f;
         sf::Event Event;
         while(Window.pollEvent(Event)) {
             if(Event.type==sf::Event::Closed) Window.close();
         }
-        Hexahedron.rotate({0.5,0,0});
+        Hexahedron.rotate({0.5*Speed,0,0});
         Warp.move(3);
         Window.clear();
         Window.draw(Warp);
@@ -50,31 +54,4 @@ int main() {
         Hexahedron.draw(&Window);
         Window.display();
     }
-    #else
-    LiveVisualizer Visualizer;
-    Visualizer.setRenderSize(Size);
-    Visualizer.setOutputSize(Size,0,1);
-    Visualizer.addPlugin(new Plugin::ConfettiCircles({1,10},{15,85},{.1,1},{sf::Color::White,{0,0,0,0}},{{255,255,255},{0,0,0,0}},{0,0},{99,0},{1,4},1.2,10),{0,1},{{{0,0,Size.x,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::Lasers(1.2,{1,0},{1,0},{2,0},{2,0},{sf::Color::White,{0,0,0,0}},10,"Product/Grad.png"),{0,2},{{{0,0,Size.x,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::VolumeCircle(4,sf::Color::Black,{255,255,255,255},2),{0,1},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::VolumeCircle(2,sf::Color::Black,{255,255,255,255},2),{0,.5},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::VolumeCircle(1,sf::Color::Black,{255,255,255,255},2),{0,.25},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::VolumeCircle(.5,sf::Color::Black,{255,255,255,255},2),{0,.125},{{{-(int)Size.y/2,0,Size.y,Size.y},0},{{Size.x-Size.y/2,0,Size.y,Size.y},0}});
-    Visualizer.addPlugin(new Plugin::Amplitude(4,1280,1,{128,128,128},{128,128,128},0,0.05,1),{0,1},{{{0,.75*Size.y,Size.x,Size.y/4},0},{{0,0,Size.x,Size.y/4},180}});
-    Visualizer.addPlugin(new Plugin::Hanning(512,sf::Color::White),{0,1},{{{0,Size.y/4,Size.x,Size.y/2},0}});
-    Visualizer.addPlugin(new Plugin::Pitch(2048,sf::Color::White,sf::Color::White,true,3),{0,1},{{{0,.75*Size.y,Size.x,Size.y/4},0},{{0,0,Size.x,Size.y/4},180}});
-    sf::Sprite Sprite;
-    Sprite.setTexture(Visualizer.getTexture());
-    Visualizer.Play();
-    while(Window.isOpen()) {
-        sf::Event Event;
-        while(Window.pollEvent(Event)) {
-            if(Event.type==sf::Event::Closed) Window.close();
-        }
-        Window.clear();
-        Visualizer.render();
-        Window.draw(Sprite);
-        Window.display();
-    }
-    #endif
 }
